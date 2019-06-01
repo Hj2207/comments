d0d8a0293bec8d5e92e541f97b0287e4072c883e
Hj2207 <tharshita13@gmail.com>
2019-06-01 13:49:11 +0530

    fix

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const bodyParser=require('body-parser');
 const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'phantom',
-    database : 'finalproject'
+    connectionString : process.env.DATABASE_URL,
+     ssl:true,
   }
 });
 
